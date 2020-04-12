deeb2442252baa643ff255c5da888ec6d8bb3751
eggman79 <eggman79@criptext.com>
2020-04-13 00:13:52 +0200

    added sqlite3

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 import os.path
 import re
 import urllib.parse
+import sqlite3
+import logging
 
 from urllib.request import Request, urlopen
 
@@ -18,10 +20,11 @@ class WordsDownloader:
 
     def _make_request(self, letters):
         url = 'https://scrabble123.pl/slowa-z-liter/' + urllib.parse.quote_plus(letters)
+        logging.info('request: %s', url)
         return Request(url, headers=self.request_header)
 
     def _get_html_for_letters(self, letters):
-        return urlopen(self._make_request(letters)).read()
+        return urlopen(self._make_request(letters), timeout=10).read()
 
     def get_words(self, letters):
         html = self._get_html_for_letters(letters)
@@ -51,53 +54,77 @@ class WordsDownloader:
 
         return words
 
-class WordsSaver:
-    def __init__(self, words_file_name, letters_file_name):
-        self.words_file_name = words_file_name
-        self.letters_file_name = letters_file_name
+class Db:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        new_db = not os.path.exists(self.db_name)
+        self.conn = sqlite3.connect(self.db_name)
 
-    def add_words_for_letters(self, words, letters):
-        if not words is None:
-            words_file = open(self.words_file_name, 'a')
-            for word in words:
-                words_file.write(word + '\n')
-            words_file.close()
+        if new_db:
+            try:
+                cur = self.conn.cursor()
+                cur.execute('create table words(word text)')
+                cur.execute('create unique index words_word on words(word)')
+                cur.execute('create table last_letters(letters text)')
+                cur.execute("insert into last_letters values('')")
+                self.conn.commit()
+            except:
+                self.conn.rollback()
+                raise Exception('database creation error')
 
-        if os.path.exists(self.letters_file_name):
-            os.remove(self.letters_file_name)
+    def last_letters(self):
+        cur = self.conn.cursor()
+        cur.execute('select letters from last_letters')
+        rows = cur.fetchall()
 
-        letters_file = open(self.letters_file_name, 'w')
-        letters_file.write(letters)
-        letters_file.close()
+        if rows is None or len(rows) != 1:
+            raise Exception('database last_letters error')
 
-    def get_last_letters(self):
-        if not os.path.exists(self.letters_file_name):
+        result = rows[0][0]
+
+        if not result:
             return None
-        letters_file = open(self.letters_file_name, 'r')
-        letters = letters_file.readline()
-        letters_file.close()
-        return letters
+
+        return result
+
+    def save_words_and_letters(self, words, letters):
+        try:
+            cur = self.conn.cursor()
+
+            if not words is None:
+                for word in words:
+                    cur.execute("insert into words(word) select '" + word + "' where not exists(select 1 from words where word = '" + word + "')")
+
+            cur.execute("update last_letters set letters = '" + letters + "'")
+            self.conn.commit()
+        except:
+            self.conn.rollback()
+            raise
 
 class Main:
     def __init__(self):
+        logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
         self.alphabet = 'aąbcćdeęfghijklłmnńoóprsśtuwyzźż'
         self.min_length = 2
         self.max_length = 32
-        self.words_saver = WordsSaver('words.txt', 'letters.txt')
-        self.last_letters = self.words_saver.get_last_letters()
-        self.omit_letters = not self.last_letters is None
         self.words_downloader = WordsDownloader(self.alphabet)
+        self.db = Db('words.db')
+        self.last_letters = self.db.last_letters()
+        self.omit_letters = not self.last_letters is None
 
     @staticmethod
     def _perm(iterable, fun, repeat=None):
         pool = tuple(iterable)
         number = len(pool)
+
         repeat = number if repeat is None else repeat
         if repeat > number:
             return
+
         indices = list(range(number))
         cycles = list(range(number, number - repeat, -1))
         fun(tuple(pool[i] for i in indices[:repeat]))
+
         while number:
             for i in reversed(range(repeat)):
                 cycles[i] -= 1
@@ -117,15 +144,18 @@ class Main:
 
         if self.omit_letters:
             if letters == self.last_letters:
+                logging.info('start from previous letters %s: ', self.last_letters)
                 self.omit_letters = False
             return
 
         words = self.words_downloader.get_words(letters)
+
         if words is None:
-            print(letters)
+            logging.info('no found words for letters: %s', letters)
         else:
-            print(str(words) + ':' + letters)
-        self.words_saver.add_words_for_letters(words, letters)
+            logging.info('found words %s for letters %s', str(words), letters)
+
+        self.db.save_words_and_letters(words, letters)
 
     def download(self):
         loop = True
@@ -136,6 +166,7 @@ class Main:
                 for i in range(self.min_length, self.max_length + 1):
                     Main._perm(self.alphabet, self._on_next_perm, i)
             except Exception as ex:
+                logging.error(ex)
                 loop = True
 
 Main().download()
